be86b17470ec51ee67e9bbfab10e52ee44ea4245
martinageradams <martinageradams@gmail.com>
2023-01-31 10:25:36 +0800

    feat: add antd

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import 'antd/dist/reset.css';
 import {QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import './index.css'
