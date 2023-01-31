90f0f293d7060489b6257924870938dbd98b0820
martinageradams <martinageradams@gmail.com>
2023-01-31 15:54:53 +0800

    feat: add react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -6,17 +6,20 @@ import {QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import store from './store'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-      <Provider store={store}>
-          <QueryClientProvider client={queryClient}>
-              <App />
-              <ReactQueryDevtools initialIsOpen={false} />
-          </QueryClientProvider>
-      </Provider>
+      <BrowserRouter>
+          <Provider store={store}>
+              <QueryClientProvider client={queryClient}>
+                  <App />
+                  <ReactQueryDevtools initialIsOpen={false} />
+              </QueryClientProvider>
+          </Provider>
+      </BrowserRouter>
   </React.StrictMode>,
 )
