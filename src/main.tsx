22587cd9882438b442ae7f45324e7df48ea4271e
martinageradams <martinageradams@gmail.com>
2023-01-31 15:30:18 +0800

    feat: add @reduxjs/toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import 'antd/dist/reset.css';
+import 'antd/dist/reset.css'
 import {QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import store from './store'
+import { Provider } from 'react-redux'
 import './index.css'
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-      <QueryClientProvider client={queryClient}>
-          <App />
-          <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
+      <Provider store={store}>
+          <QueryClientProvider client={queryClient}>
+              <App />
+              <ReactQueryDevtools initialIsOpen={false} />
+          </QueryClientProvider>
+      </Provider>
   </React.StrictMode>,
 )
